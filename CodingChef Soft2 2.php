db3949cc4b94b32b663d84aa8fb56d1a0d9ad3e3
BookerGray <121211028+BookerGray@users.noreply.github.com>
2023-05-21 23:02:42 -0700

    Add files via upload

--- CodingChef Soft2 2.php
+++ CodingChef Soft2 2.php
@@ -131,7 +131,8 @@ class _ListViewPageState extends State<ListViewPage> {
       body: ListView.builder(
         itemCount: titleList.length,
         itemBuilder: (context, index) {
-          return InkWell( // https://api.flutter.dev/flutter/material/InkWell-class.html
+          // https://api.flutter.dev/flutter/material/InkWell-class.html
+          return InkWell( 
             onTap: () {
               debugPrint(titleList[index]);
               showPopup(context, titleList[index], imageList[index],
